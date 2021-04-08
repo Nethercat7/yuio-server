4d5cc84359312bb13e593de5b6bdafe8ca136d3a
Nethercat7 <1209322690@qq.com>
2021-04-08 18:11:58 +0800

    initial

---
+++ src/main/java/com/zfy/yuio/YuioApplication.java
@@ -0,0 +1,13 @@
+package com.zfy.yuio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zfy/yuio/YuioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zfy.yuio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
