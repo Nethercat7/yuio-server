1bf3e0b29c097d7539c9d95212298f472ef2d592
Nethercat7 <1209322690@qq.com>
2021-04-09 23:06:33 +0800

    组织管理模块，添加和更新功能添加PID为空时判断

--- src/main/java/com/zfy/yuio/service/impl/OrgServiceImpl.java
+++ src/main/java/com/zfy/yuio/service/impl/OrgServiceImpl.java
@@ -25,7 +25,8 @@ public class OrgServiceImpl implements OrgService {
     @Override
     public int add(Org org) {
         org.setOrgId(snowflakeIdGeneratorUntil.getId());
-        if (org.getOrgId() == null) {
+        if (org.getOrgPid() == null) {
+            org.setOrgPid("0");
             org.setOrgLevel(0);
         } else {
             Org o = orgDao.getById(org.getOrgPid());
@@ -38,12 +39,14 @@ public class OrgServiceImpl implements OrgService {
     public List<Org> get() {
         List<Org> org = orgDao.get();
         List<Org> orgList=new ArrayList<>();//存放最上级的Org
+        //遍历，筛选出所有的最上级Org
         for (Org o:org
              ) {
             if(o.getOrgPid().equals("0")){
                 orgList.add(o);
             }
         }
+        //为上级设置children
         for (Org o:orgList
              ) {
             o.setChildren(getChildren(o.getOrgId(),org));
@@ -58,23 +61,24 @@ public class OrgServiceImpl implements OrgService {
 
     @Override
     public int upd(Org org) {
+        if(org.getOrgPid()==null) org.setOrgPid("0");
         return orgDao.upd(org);
     }
 
     private List<Org> getChildren(String pid,List<Org> list){
+        //用于存放children
         List<Org> children=new ArrayList<>();
+        //遍历，将当前对象的pid与参数中pid匹配的对象存入list中
         for (Org o:list
              ) {
             if(o.getOrgPid().equals(pid)){
                 children.add(o);
             }
         }
+        //为当前的children list中的对象设置children，重复上面的步骤
         for (Org o:children
              ) {
             o.setChildren(getChildren(o.getOrgId(),list));
-        }
-        if(children.size()==0){
-            return null;
         }
         return children;
     }
