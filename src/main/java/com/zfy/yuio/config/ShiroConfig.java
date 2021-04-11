2a32c2a9722bfff542d6cfbe6f41ee5c714bd8b4
Nethercat7 <1209322690@qq.com>
2021-04-11 22:52:38 +0800

    搭建Shrio基本框架

---
+++ src/main/java/com/zfy/yuio/config/ShiroConfig.java
@@ -0,0 +1,39 @@
+package com.zfy.yuio.config;
+
+import com.zfy.yuio.realm.AuthRealm;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+   //创建Filter
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager defaultWebSecurityManager){
+        //创建Filter
+        ShiroFilterFactoryBean shiroFilterFactoryBean=new ShiroFilterFactoryBean();
+        //注入安全管理器
+        shiroFilterFactoryBean.setSecurityManager(defaultWebSecurityManager);
+        //受限资源
+        //公共资源
+        return shiroFilterFactoryBean;
+    }
+
+    //创建Web管理器
+    @Bean
+    public DefaultWebSecurityManager defaultWebSecurityManager(Realm realm){
+        //创建管理器
+        DefaultWebSecurityManager defaultWebSecurityManager=new DefaultWebSecurityManager();
+        //设置Realm
+        defaultWebSecurityManager.setRealm(realm);
+        return defaultWebSecurityManager;
+    }
+
+    //创建自定义Realm
+    @Bean
+    public Realm realm(){
+        return new AuthRealm();
+    }
+}
