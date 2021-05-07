96ba39a46e47a4afbd98266da1b599a110ee7559
Nethercat7 <1209322690@qq.com>
2021-05-07 23:14:40 +0800

    修改获取用户权限标识符SQL

--- src/main/java/com/zfy/yuio/realms/AuthRealm.java
+++ src/main/java/com/zfy/yuio/realms/AuthRealm.java
@@ -40,7 +40,14 @@ public class AuthRealm extends AuthorizingRealm {
         SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
         String token = (String) principalCollection.getPrimaryPrincipal();
         Long id = JWTUtil.getId(token);
-        List<String> perms=permsService.getUserPerms(id);
+        String type = JWTUtil.getType(token);
+        List<String> perms;
+        //获取使用者拥有的权限
+        if (type.equals("usr")) {
+            perms = permsService.getUserPerms(id);
+        } else {
+            perms = permsService.getStudentPerms(id);
+        }
         simpleAuthorizationInfo.addStringPermissions(perms);
         return simpleAuthorizationInfo;
     }
