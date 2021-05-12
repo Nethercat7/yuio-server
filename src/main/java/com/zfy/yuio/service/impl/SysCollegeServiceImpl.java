aa276d87e8e1c757563603687a4a413784f35500
Nethercat7 <1209322690@qq.com>
2021-05-12 18:30:24 +0800

    院系数据输入重复性校验

--- src/main/java/com/zfy/yuio/service/impl/SysCollegeServiceImpl.java
+++ src/main/java/com/zfy/yuio/service/impl/SysCollegeServiceImpl.java
@@ -7,6 +7,7 @@ import com.zfy.yuio.service.SysCollegeService;
 import com.zfy.yuio.utils.SnowflakeIdGeneratorUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
@@ -24,8 +25,12 @@ public class SysCollegeServiceImpl implements SysCollegeService {
 
     @Override
     public int add(SysCollege params) {
-        params.setCollegeId(snowflakeIdGeneratorUtil.nextId());
-        return collegeDao.add(params);
+        int status = validator(params, 0);
+        if (status == 0) {
+            params.setCollegeId(snowflakeIdGeneratorUtil.nextId());
+            collegeDao.add(params);
+        }
+        return status;
     }
 
     @Override
@@ -40,7 +45,11 @@ public class SysCollegeServiceImpl implements SysCollegeService {
 
     @Override
     public int upd(SysCollege params) {
-        return collegeDao.upd(params);
+        int status = validator(params, 1);
+        if (status == 0) {
+            collegeDao.upd(params);
+        }
+        return status;
     }
 
     @Override
@@ -50,10 +59,22 @@ public class SysCollegeServiceImpl implements SysCollegeService {
 
     @Override
     public void addFromExcel(List<ExcelCollege> params) {
-        for (ExcelCollege c:params
-             ) {
+        for (ExcelCollege c : params
+        ) {
             c.setCollegeId(snowflakeIdGeneratorUtil.nextId());
         }
         collegeDao.addFromExcel(params);
+    }
+
+    public int validator(SysCollege params, int type) {
+        if (type == 0) {
+            if (!ObjectUtils.isEmpty(collegeDao.getIdByName(params.getCollegeName()))) return 1;
+        } else {
+            SysCollege data = collegeDao.getById(params.getCollegeId());
+            if (!data.getCollegeName().equals(params.getCollegeName())) {
+                if (!ObjectUtils.isEmpty(collegeDao.getIdByName(params.getCollegeName()))) return 1;
+            }
+        }
+        return 0;
     }
 }
